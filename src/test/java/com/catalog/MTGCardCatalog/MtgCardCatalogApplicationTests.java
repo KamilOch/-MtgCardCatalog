8c421017e1d13a48c0f550ad85504c3c5511a248
Kamil Ochnik <ilmak@o2.pl>
2020-02-03 12:17:49 +0100

    initial commit

---
+++ src/test/java/com/catalog/MTGCardCatalog/MtgCardCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catalog.MTGCardCatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtgCardCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
